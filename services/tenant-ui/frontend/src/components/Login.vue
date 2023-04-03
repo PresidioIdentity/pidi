bffea7157ef1144e1b3229ecc4fdaf10a1cae3d9
KhallilB <khallil.bailey@presidioidentity.com>
2023-04-03 14:46:03 -0700

    img height
    Signed-off-by: KhallilB <khallil.bailey@presidioidentity.com>

--- services/tenant-ui/frontend/src/components/Login.vue
+++ services/tenant-ui/frontend/src/components/Login.vue
@@ -1,4 +1,4 @@
-<template>
+heigh<template>
   <div class="traction-login grid w-screen h-screen">
     <div class="col-12 md:col-6 xl:col-6">
       <div class="px-8">
@@ -150,7 +150,7 @@ const doGoBack = () => {
   align-items: center;
 
   img {
-    height: 30em;
+    height: 45vh;
   }
 }
 </style>
